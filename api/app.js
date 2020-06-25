9a09897083f1938c93db124483bc58dca9917b08
Kyle C <kylecilia@gmail.com>
2020-06-24 22:16:20 -0700

    Adds test API in /api/routes/test.js/. Also adds to app.js, as shown by comment anchors/

--- api/app.js
+++ api/app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// NOTE :: TEST
+//-------
+const testAPI = require('./routes/test');
+//-------
+
 var app = express();
 
 // view engine setup
@@ -21,6 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// NOTE :: test
+//-------
+app.use('/test', testAPI);
+//-------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
