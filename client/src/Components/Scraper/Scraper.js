977b403cf06e49db2131034b0369fb64837621ac
Kyle C <kylecilia@gmail.com>
2020-07-12 10:46:08 -0700

    Outputs scraped articles that are stored in state array in Scraper.js

--- client/src/Components/Scraper/Scraper.js
+++ client/src/Components/Scraper/Scraper.js
@@ -6,8 +6,10 @@ const Scraper = () => {
 
 	const testScraper = async () => {
 		try {
+			// gets response from API endpoint '/scrape/
 			const response = await API.scrape();
 			console.log('response.data::', ...response.data);
+			// sets state to response
 			setState([...response.data]);
 		} catch (err) {
 			console.log('error in Scraper.js');
@@ -16,17 +18,32 @@ const Scraper = () => {
 	};
 
 	const printState = () => {
-
-	}
+		// iterating through state array
+		for (let i = 0; i < state.length; i++) {
+			console.log(`Headline:: ${state[i].headline}
+			\nSummary:: ${state[i].summary}`);
+		}
+	};
 
 	useEffect(() => {
 		testScraper();
 	}, []);
 
-return (
-	<div>
-		{console.log(state)}
-	</div>);
+	return (
+		<div>
+			{/* maps through array */}
+			{state.map((article) => {
+				return (
+					<div>
+						{/* outputs headline, summary, and link from state array */}
+						<h2>{article.headline}</h2>
+						<p>{article.summary}</p>
+						<a href={article.link}>{article.link}</a>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Scraper;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,26 +12,39 @@ router.get('/scrape', async (req, res) => {
 		// console.log(data.data);
 		let $ = cheerio.load(data.data);
 		let results = [];
+		// scrapes through the #collection-world id
 		$('#collection-world')
+			// finds each article element
 			.find('article')
+			// scrapes through each article element, using elem as 'each'
 			.each(function (i, elem) {
+				// grabs h2 element for the headline
 				let headline = $(elem).find('h2').text();
+				// grabs p element for the summary
 				let summary = $(elem).find('p').text();
+				// grabs image element for image
 				let imgSrc = $(elem).find('img').attr('src');
+				// grabs a element and url fragment for link
 				let link = $(elem).find('a').attr('href');
+
+				//NOTE :: Debug
 				console.log('Headline::', headline);
 				console.log('Summary::', summary);
 				console.log('Link::', link);
 				console.log('imgSrc::', imgSrc);
 				console.log('------------------------------');
+
+				// gathers all elements and pushes it to the results array
 				results.push({
 					headline,
 					summary,
 					imgSrc,
-					link: url+link
+					link: 'https://nytimes.com/' + link,
 				});
 			});
 		console.log(results);
+
+		// passes results back to client
 		res.send(results);
 	} catch (error) {
 		console.log(error);
